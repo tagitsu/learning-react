d67e455504e6e94f8997a6330df740ac4e51e069
Julia Diaków <tagitsu@gmail.com>
2022-11-24 10:57:33 +0100

    13.2 first task - create and use redux store

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,24 @@
 import styles from './Column.module.scss';
 import Card from '../Card/Card';
 import CardForm from '../CardForm/CardForm';
+import { useSelector } from 'react-redux';
 
-const Column = props => (
+const Column = props => {
+
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
+  return(
     <article className={styles.column}>
       <h3 className={styles.title}>
         <span className={styles.icon + ' fa fa-' + props.icon}></span>
         {props.title}
       </h3>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+        {cards.map(card => <Card key={card.id} title={card.title} />)}
       </ul>
       <CardForm columnId={props.id} action={props.action} />
     </article>
   );
+}
 
 export default Column;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
-
-ReactDOM.render(
-  <App />,
-  document.querySelector('#root')
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
 );
