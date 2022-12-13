be3715922deea53c515ddbc80be489e29928abfd
Julia Diaków <tagitsu@gmail.com>
2022-12-13 18:28:15 +0100

    Fix event listener taht dispatches toggle action

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,19 @@
 import styles from './Card.module.scss';
 import clsx from 'clsx';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { getCard } from '../../redux/cardsRedux';
-import toggleCardFavorite from '../../redux/cardsReducer';
+import { toggleCardFavorite } from '../../redux/cardsReducer';
 
 const Card = props => {
 
+  const dispatch = useDispatch();
+
   const card = useSelector(state => getCard(state.cards, props.id));
   console.log('card - card', card.id);
-  
 
   return(
     <li className={styles.card}>{props.title}
-      <button className={styles.card__button} onClick={() => toggleCardFavorite(card.id)}>
+      <button className={styles.card__button} onClick={() => dispatch(toggleCardFavorite(card.id))}>
         <i className={clsx('fa', 'fa-star-o', styles.btn, card.isFavorite && styles.liked)} />
       </button>
     </li>
